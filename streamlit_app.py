227238fc7e6d39cff364e4c1cdde63f2760c08bd
haryaka <164843734+haryaka@users.noreply.github.com>
2024-11-28 20:09:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine Learning App')
 
 st.info('Ini adalah Machine Learning untuk memenuhi project UAS mata kuliah Data Mining')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
