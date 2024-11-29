c86b11ec0ef8f85ac803e3cbcce2d6225ea1406e
haryaka <164843734+haryaka@users.noreply.github.com>
2024-11-29 11:40:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,6 +72,7 @@ with st.expander('Data preparation'):
   st.write('**Encoded y**')
   y
 
+
 # Model training and inference
 ## Train the ML model
 clf = RandomForestClassifier()
@@ -80,5 +81,13 @@ clf.fit(X, y)
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
+
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ('Adelie', 'Chinstrap', 'Gentoo')
+df_prediction_proba.rename(columns={0: 'Adelie',
+                                    1: 'Chinstrap',
+                                    2: 'Gentoo'})
+
+
 
 prediction_proba
